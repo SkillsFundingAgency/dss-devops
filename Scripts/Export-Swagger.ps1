73f4ec6abd7e13ca6a9f7ccbbb32cf450b6d0368
Nick Graham101 <nick.graham101@googlemail.com>
2018-06-13 14:54:05 +0100

    Increased depth of ConvertTo-Json from 2 to 20

--- Scripts/Export-Swagger.ps1
+++ Scripts/Export-Swagger.ps1
@@ -15,13 +15,13 @@ param(
 Import-Module $ModulePath
 
 $Swagger = Get-Swagger -FunctionAppName $FunctionAppName -ApiResourceName $ApiResourceName -ResourceGroupName $ResourceGroup
-Write-Verbose -Message $($Swagger | ConvertTo-Json)
+Write-Verbose -Message $($Swagger | ConvertTo-Json -Depth 20)
 
 $FileName = "$($FunctionAppName)_swagger-def_$([DateTime]::Now.ToString("yyyyMMdd-hhmmss")).json"
 Write-Verbose -Message "Filename: $FileName"
 
 $OutputFile = New-Item -Path $OutputFilePath -Name $FileName
-Set-Content -Path $OutputFile.FullName -Value ($Swagger | ConvertTo-Json)
+Set-Content -Path $OutputFile.FullName -Value ($Swagger | ConvertTo-Json -Depth 20)
 
 # store the filename in a VSTS variable to be consumed by a later task
 Write-Output "##vso[task.setvariable variable=SwaggerFile;]$FileName"
