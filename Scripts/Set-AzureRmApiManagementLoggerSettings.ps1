8efc5aea9ac91979f26826e5836f3ed90cab3253
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-02 16:06:43 +0100

    Extended Set-AzureRmApiManagementLoggerSettings.ps1

--- Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
+++ Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
@@ -1,6 +1,21 @@
 [CmdletBinding()]
 param(
+    [Parameter(Mandatory=$true)]
+    [string]$SubscriptionId,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$ResourceGroupName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$ApimInstanceName,
+    
+    [Parameter(Mandatory=$true)]
+    [string]$ApimLoggerId,
+    
+    [Parameter(Mandatory=$true)]
     [Guid]$ApplicationId,
+    
+    [Parameter(Mandatory=$true)]
     [System.Security.SecureString]$AppPassword
 )
 
@@ -9,3 +24,9 @@ Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Helpers.psm1).Path
 $SecurePassword = ConvertTo-SecureString $AppPassword -Force -AsPlainText
 
 $BearerToken = Get-AzureApiBearerToken -ApplicationId $ApplicationId -AppPassword $SecurePassword
+
+$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/diagnostics/$ApimLoggerId?api-version=2018-06-01-preview"
+
+Invoke-RestMethod -Method GET -Uri $Uri -Headers @{Authorization = "Bearer $($BearerToken.access_token)"}
+
+#$Uri = PUT https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/diagnostics/{diagnosticId}?api-version=2018-06-01-preview
