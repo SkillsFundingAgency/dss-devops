33cc776d1761cc7befb371e1310eae8814c94887
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-02 16:23:02 +0100

    Added logging to Set-AzureRmApiManagementLoggerSettings

--- Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
+++ Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
@@ -26,10 +26,12 @@ Import-Module (Resolve-Path -Path $PSScriptRoot\..\Modules\Helpers.psm1).Path
 
 $SecurePassword = ConvertTo-SecureString $AppRegistrationKey -Force -AsPlainText
 
+Write-Verbose "Requesting bearer token"
 $BearerToken = Get-AzureApiBearerToken -TenantId $TenantId -ApplicationId $ApplicationId -AppRegistrationKey $SecurePassword
+Write-Verbose "Bearer token obtained`n$($BearerToken.access_token)"
 
 $Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/diagnostics/$ApimLoggerId?api-version=2018-06-01-preview"
-
+Write-Verbose "Calling Azure REST API method`n$Uri"
 Invoke-RestMethod -Method GET -Uri $Uri -Headers @{Authorization = "Bearer $($BearerToken.access_token)"}
 
 #$Uri = PUT https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/diagnostics/{diagnosticId}?api-version=2018-06-01-preview
