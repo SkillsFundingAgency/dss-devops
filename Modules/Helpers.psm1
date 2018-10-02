e8729c39192d9563f2b5fa1bf2ab4736730c88a2
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-02 13:41:42 +0100

    Added Get-AzureApiBearerToken func to Helpers module

--- Modules/Helpers.psm1
+++ Modules/Helpers.psm1
@@ -1,3 +1,54 @@
+function Get-AzureApiBearerToken {
+<#    .SYNOPSIS
+
+    .DESCRIPTION
+
+    .EXAMPLE
+
+    .NOTES
+#>    
+    [CmdletBinding()]
+    param (
+      [string]$SubscriptionName,
+      [Guid]$ApplicationId,
+      [System.Security.SecureString]$AppPassword
+    )
+  
+    Write-Host "Logging preference: $VerbosePreference"
+  
+    $Subscription = Get-AzureRmSubscription -SubscriptionName $SubscriptionName
+    
+    Write-Verbose -Message "SubscriptionId: $($Subscription.Id)"
+   
+    $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($AppPassword)
+    $UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+  
+    $Body = @{
+      'resource'= "https://management.core.windows.net/"
+      'client_id' = $ApplicationId
+      'grant_type' = 'client_credentials'
+      'client_secret' = $UnsecurePassword
+    }
+    Write-Debug $Body
+  
+    $TokenEndpoint = {https://login.windows.net/{0}/oauth2/token} -f $Subscription.TenantId
+    Write-Verbose $TokenEndpoint
+  
+    $Params = @{
+        ContentType = 'application/x-www-form-urlencoded'
+        Headers = @{'accept'='application/json'}
+        Body = $Body
+        Method = 'Post'
+        URI = $TokenEndpoint
+        UseBasicParsing = $true
+    }
+    Write-Debug $Params
+  
+    $Token = Invoke-RestMethod @Params
+    # Show the raw token and expiration date converted in readable format: #
+    $Token | Select-Object *, @{L='Expires';E={[timezone]::CurrentTimeZone.ToLocalTime(([datetime]'1/1/1970').AddSeconds($_.expires_on))}} | Format-List *
+  }
+
 function New-Password {
     <#
 
