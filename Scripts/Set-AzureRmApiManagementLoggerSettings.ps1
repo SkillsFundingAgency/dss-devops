37377abbd2ba4c52cabc9ce0452bff0f4cda093b
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-02 17:00:07 +0100

    Changed method to get named API

--- Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
+++ Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
@@ -41,9 +41,7 @@ Write-Verbose "Requesting bearer token"
 $BearerToken = Get-AzureApiBearerToken -TenantId $TenantId -ApplicationId $ApplicationId -AppRegistrationKey $SecurePassword
 Write-Verbose "Bearer token obtained`n$($BearerToken.access_token)"
 
-#$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/diagnostics/$($ApimLoggerId)?api-version=2018-06-01-preview"
-#$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/diagnostics?api-version=2018-06-01-preview"
-$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/apis/$ApiName/diagnostics?api-version=2018-06-01-preview"
+$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/apis/$ApiName/diagnostics/$($ApimLoggerId)?api-version=2018-06-01-preview"
 Write-Verbose "Calling Azure REST API method`n$Uri"
 Invoke-RestMethod -Method GET -Uri $Uri -Headers @{Authorization = "Bearer $($BearerToken.access_token)"}
 
