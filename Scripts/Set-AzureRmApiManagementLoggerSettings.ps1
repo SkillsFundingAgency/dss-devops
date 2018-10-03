ccdb9c61e04a2eed55bb7cbbe074c9024a2f5878
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-03 10:41:37 +0100

    Modified Set-AzureRmApiManagementLoggerSettings to PUT data

--- Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
+++ Scripts/Set-AzureRmApiManagementLoggerSettings.ps1
@@ -23,6 +23,16 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$ApimLoggerId,
 
+    <#The name of the logger for this API, if created using an ARM template it will normally be set to applicationinsights.
+    This property isn't exposed in the Portal or via PowerShell but can be found at https://resources.azure.com by navigating to
+    https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroup}/providers/Microsoft.ApiManagement/service/{apimInstance}/apis/{apiName}/diagnostics?api-version=2018-01-01
+    The name is the last section of the id property#>
+    [Parameter(Mandatory=$true)]
+    [string]$ApimApiLoggerId,
+
+    [Parameter(Mandatory=$true)]
+    [int]$SamplingPercentage,
+
     [Parameter(Mandatory=$true)]
     [Guid]$TenantId,
 
@@ -41,8 +51,50 @@ Write-Verbose "Requesting bearer token"
 $BearerToken = Get-AzureApiBearerToken -TenantId $TenantId -ApplicationId $ApplicationId -AppRegistrationKey $SecurePassword
 Write-Verbose "Bearer token obtained`n$($BearerToken.access_token)"
 
-$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/apis/$ApiName/diagnostics/$($ApimLoggerId)?api-version=2018-06-01-preview"
-Write-Verbose "Calling Azure REST API method`n$Uri"
-Invoke-RestMethod -Method GET -Uri $Uri -Headers @{Authorization = "Bearer $($BearerToken.access_token)"}
+$Body = @{
+    "properties" = @{
+        "alwaysLog" = "allErrors"
+        "loggerId" = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApiName/loggers/$ApimLoggerId"
+        "sampling" = @{
+            "samplingType" = "fixed"
+            "percentage" = $SamplingPercentage
+        }
+        "frontend" = @{
+            "request" = @{
+              "headers" = @(
+              )
+              "body" = @{
+                "bytes" = 0
+              }
+            }
+            "response"= @{
+              "headers"= @(
+              )
+              "body"= @{
+                "bytes"= 0
+              }
+            }
+        }
+        "backend"= @{
+            "request"= @{
+                "headers"= @(
+                )
+                "body"= @{
+                    "bytes"= 0
+                }
+            }
+            "response"= @{
+                "headers"= @(
+                )
+                "body"= @{
+                    "bytes"= 0
+                }
+            }
+        }
+    }
+}
 
-#$Uri = PUT https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.ApiManagement/service/{serviceName}/diagnostics/{diagnosticId}?api-version=2018-06-01-preview
+
+$Uri = "https://management.azure.com/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupName/providers/Microsoft.ApiManagement/service/$ApimInstanceName/apis/$ApiName/diagnostics/$($ApimApiLoggerId)?api-version=2018-06-01-preview"
+Write-Verbose "Calling Azure REST API method`n$Uri"
+Invoke-RestMethod -Method PUT -Uri $Uri -Headers @{Authorization = "Bearer $($BearerToken.access_token)"} -Body $Body -ContentType "application/json"
