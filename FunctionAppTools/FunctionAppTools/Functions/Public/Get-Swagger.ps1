34da8800b4f88b235ebfa1333bd1554cb6e8b3ff
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-03 16:46:04 +0100

    Made authentication optional on Get-Swagger

--- FunctionAppTools/FunctionAppTools/Functions/Public/Get-Swagger.ps1
+++ FunctionAppTools/FunctionAppTools/Functions/Public/Get-Swagger.ps1
@@ -1,7 +1,7 @@
 
 function Get-Swagger{
 <#
-    . SUMMARY
+    .SUMMARY
     Calls api-defintion method in DSS function apps to return the swagger definition for that resource
     .NOTES
     Depends on Invoke-AzureRmResourceAction (called in private function Get-PublishingProfileCredentialsAzure) which runs only when logged on with a Service Principal
@@ -15,10 +15,14 @@ function Get-Swagger{
         [Parameter(Mandatory=$true)]
         [string]$ApiResourceName,
         [Parameter(Mandatory=$true)]
-        [string]$ResourceGroupName
+        [string]$ResourceGroupName,
+        [Parameter(Mandatory=$false)]
+        [Switch]$AuthenticationKeyRequired
     )
 
-    $MasterKey = Get-FunctionAppMasterkey -ResourceGroupName $ResourceGroupName -FunctionAppName $FunctionAppName -FunctionAppDomain $FunctionAppDomain
+    if($AuthenticationKeyRequired.IsPresent) {
+        $MasterKey = Get-FunctionAppMasterkey -ResourceGroupName $ResourceGroupName -FunctionAppName $FunctionAppName -FunctionAppDomain $FunctionAppDomain
+    }
     
     $Uri = "https://$FunctionAppName.$FunctionAppDomain/api/$ApiResourceName/api-definition?code=$($MasterKey)"
     $Swagger = Invoke-RestMethod -Method GET -Uri $Uri -UseBasicParsing
