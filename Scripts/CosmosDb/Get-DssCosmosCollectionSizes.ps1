b30a9680ecef3748f0497653f34a4bfbd35074eb
Nick Graham101 <nick.graham101@googlemail.com>
2018-10-26 15:32:03 +0100

    Added cosmos scripts

---
+++ Scripts/CosmosDb/Get-DssCosmosCollectionSizes.ps1
@@ -0,0 +1,27 @@
+<#
+
+    .SUMMARY
+    Gets the document count from all the collections contained within the DSS CosmosDb account
+
+    .NOTES
+    Requires https://cosmosdbportalstorage.blob.core.windows.net/datamigrationtool/2018.02.28-1.8.1/dt-1.8.1.zip to be extracted to C:\Program Files (x86)\AzureCosmosDBDataMigrationTool\
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$CosmosAccountName,
+    [Parameter(Mandatory=$false)]
+    [array]$Databases = @("actionplans", "actions", "addresses", "contacts", "customers", "diversitydetails", "goals", "interactions", "outcomes", "sessions", "subscriptions", "transfers", "webchats"),
+    [Parameter(Mandatory=$true)]
+    [string]$SecondaryCosmosKey
+)
+
+$SecureKey = ConvertTo-SecureString -String $SecondaryCosmosKey -AsPlainText -Force
+
+foreach ($Database in $Databases) {
+
+    $CosmosDbContext = New-CosmosDbContext -Account $CosmosAccountName  -Database $Database -Key $SecureKey
+    $CollectionSize = Get-CosmosDbCollectionSize -Context $CosmosDbContext -Id $Database
+    "$Database : $($CollectionSize.documentsCount)"
+
+}
---
+++ Scripts/CosmosDb/Remove-DssCosmosDbCollectionDocuments.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$CosmosAccountName,
+    [Parameter(Mandatory=$false)]
+    [array]$Databases = @("actionplans", "actions", "addresses", "contacts", "customers", "diversitydetails", "goals", "interactions", "outcomes", "sessions", "subscriptions", "transfers", "webchats"),
+    [Parameter(Mandatory=$true)]
+    [string]$SecondaryCosmosKey
+)
+
+$SecureKey = ConvertTo-SecureString -String $SecondaryCosmosKey -AsPlainText -Force
+
+Write-Host "Warning you are about to delete the following collections:`n$($Databases -join "`n")" -ForegroundColor Red
+$Continue = Read-Host -Prompt "`nContinue (Y/n)?"
+
+if ($Continue -eq "Y") {
+
+    foreach ($Database in $Databases) {
+
+        $CosmosDbContext = New-CosmosDbContext -Account $CosmosAccountName  -Database $Database -Key $SecureKey
+        
+        Write-Host "Getting documents from $Database"
+        $Docs = Get-CosmosDbDocument -Context $CosmosDbContext -CollectionId $Database
+        Write-Host "Retrieved $($docs.Count) documents, deleting documents"
+        foreach ($Doc in $Docs) {
+
+            Remove-CosmosDbDocument -Context $CosmosDbContext -CollectionId $Database -Id $Doc.id
+
+        }
+        $Docs = Get-CosmosDbDocument -Context $CosmosDbContext -CollectionId $Database
+        Write-Host "$($docs.Count) documents remaining"
+    
+    }
+
+}
+
+
+
