785b927b56b27e21a0415b84ebb4a9135e4ce5a1
NickGraham101 <nick.graham101@googlemail.com>
2019-02-14 15:24:35 +0000

    Made Test-BranchName output dependent on pipeline type

--- Scripts/Test-BranchName.ps1
+++ Scripts/Test-BranchName.ps1
@@ -1,7 +1,10 @@
 [CmdletBinding()]
 param(
     [Parameter(Mandatory=$true)]
-    [string]$BranchName
+    [string]$BranchName,
+    [ValidateSet("Build", "Release")]
+    [Parameter(Mandatory=$true)]
+    [string]$PipelineType
 )
 
 # --- RegEx Patterns
@@ -11,28 +14,39 @@ $V1FeatureBranchRegEx = "^CDS-\d+-\w+[-v\d]{0}$"
 $V2OrHigherMasterBranchRegEx = "^master-v\d$"
 $V2OrHigherFeatureBranchRegEx = "^CDS-\d+-\w+-v\d$"
 
+if ($PipelineType -eq "Build") {
+
+    $IsOutput = "true"
+
+}
+elseif ($PipelineType -eq "Release") {
+
+    $IsOutput = "false"
+
+}
+
 if ($BranchName -match $V1MasterBranchRegEx) {
 
     Write-Verbose -Message "$BranchName is a a version 1 master branch"
-    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=true]Version1"
+    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version1"
 
 }
 elseif ($BranchName -match $V1FeatureBranchRegEx) {
 
     Write-Verbose -Message "$BranchName is a version 1 feature branch"
-    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=true]Version1"
+    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version1"
 
 }
 elseif ($BranchName -match $V2OrHigherMasterBranchRegEx) {
     
     Write-Verbose -Message "$BranchName is a version 2 or higher master branch"
-    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=true]Version2+"
+    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version2+"
 
 }
 elseif ($BranchName -match $V2OrHigherFeatureBranchRegEx) {
 
     Write-Verbose -Message "$BranchName is a version 2 or higher feature branch"
-    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=true]Version2+"
+    Write-Host "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version2+"
 
 }
 else {
