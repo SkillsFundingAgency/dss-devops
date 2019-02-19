6d6ef5dc69c4e4ba4515b9264ec2508a06a466b9
Nick Graham <34914709+NickGraham101@users.noreply.github.com>
2019-02-19 13:00:06 +0000

    Cds 1156 yaml build definition (#11)
    * Added Test-BranchName script
    
    * Made Test-BranchName output dependent on pipeline type
    
    * Added test for invalid branch names
    
    * Changed write-host to write-output
    
    * Added remaining unit tests for Test-BranchName
    
    * Modified Test-BranchName to write func app version to VSTS
    
    * Hardcoded isOutput on write func app name to false
    
    * Fixed regex and tests
    
    * Refactored to reference apim child resources directly
    
    * Removed apim service depends on
    
    * Added version set to apim template
    
    * Fixed version name and type
    
    * Fixed dependancies
    
    * Added apiVersionSetId prop
    
    * Fixed resourceId call
    
    * Added missing param to resourceId
    
    * Removed redundant params
    
    * Added additional output to set DssApiVersion
    
    * Changed how API path retrieved
    
    * Removed property when setting $Api value
    
    * Modified Test-BranchName to handle PR branches
    
    * Made PullRequestBranchName param optional
    
    * Added tests for Test-BranchName
    
    * Added yaml build template
    
    * Lowered code covergage
    
    * Added help text

---
+++ Scripts/Test-BranchName.ps1
@@ -0,0 +1,175 @@
+<#
+    .SYNOPSIS
+    Checks that a branch name confirms to agreed DSS naming convention and writes variables to Azure DevOps Pipelines to be consumed by later steps
+
+    .EXAMPLE
+    Test-BranchName -BranchName $(Build.SourceBranchName) -PullRequestBranchName $(if ("$(Build.SourceBranchName)" -eq "merge") { "$(System.PullRequest.SourceBranch)" } else { "NotAPullRequest" } ) -PipelineType Build -Verbose
+
+    .PARAMETER BranchName
+    The name of a branch
+
+    .PARAMETER PullRequestBranchName
+    Required if Build definition will be triggered by Pull Requests.  See example for how to implement this.
+
+    .PARAMETER PipelineType
+    Either Build or Release
+
+    .PARAMETER FunctionAppBaseName
+    The function app name minus the version, eg dss-at-cust-fa rather than dss-at-cust-v2-fa
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$BranchName,
+    [Parameter(Mandatory=$false)]
+    [string]$PullRequestBranchName,
+    [ValidateSet("Build", "Release")]
+    [Parameter(Mandatory=$true)]
+    [string]$PipelineType,
+    [Parameter(Mandatory=$false, ParameterSetName="Release")]
+	[string]$FunctionAppBaseName
+)
+
+function Write-FunctionAppName {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string[]]$NameParts,
+        [Parameter(Mandatory=$true, ParameterSetName="Version1")]
+        [switch]$Version1,
+        [Parameter(Mandatory=$true, ParameterSetName="Version2+")]
+        $RegExMatches
+    )
+ 
+    if ($Version1.IsPresent) {
+
+        $FunctionAppName = ($NameParts[0..2] -join "-"), "v1", $NameParts[3] -join "-"
+
+    }
+    else {
+
+        $FunctionAppName = ($NameParts[0..2] -join "-"), $RegExMatches[1], $NameParts[3] -join "-"
+
+    }
+
+    Write-Verbose -Message "Setting FunctionAppName to $FunctionAppName"
+    $Output = "##vso[task.setvariable variable=FunctionAppName;isOutput=false]$FunctionAppName"
+    $Output
+
+}
+
+# --- RegEx Patterns
+$V1MasterBranchRegEx = "^master$"
+$V1FeatureBranchRegEx = "^CDS-\d+-\w+[-v\d]{0}$"
+
+$V2OrHigherMasterBranchRegEx = "^master-(v\d+)$"
+$V2OrHigherFeatureBranchRegEx = "^CDS-\d+-\w+-(v\d+)$"
+
+if ($PSCmdlet.ParameterSetName -eq "Release") {
+
+    $Result = $FunctionAppBaseName | Select-String -Pattern "^[a-z]+-[a-z]+-[a-z]+-fa$"
+    if (!$Result) {
+    
+        throw "Warning: FunctionAppBaseName variable not set correctly, value $FunctionAppBaseName must match regex ^[a-z]+-[a-z]+-[a-z]+-fa$, eg dss-at-cust-fa"
+    
+    }
+    else {
+    
+        $NameParts = $FunctionAppBaseName -split "-"
+    
+    }  
+
+}
+
+if ($PipelineType -eq "Build") {
+
+    $IsOutput = "true"
+
+}
+elseif ($PipelineType -eq "Release") {
+
+    $IsOutput = "false"
+
+}
+
+if ($BranchName -match $V1MasterBranchRegEx) {
+
+    Write-Verbose -Message "$BranchName is a a version 1 master branch"
+    Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version1"
+    if ($PSCmdlet.ParameterSetName -eq "Release") {
+
+        Write-Output "##vso[task.setvariable variable=DssApiVersion;isOutput=$IsOutput]v1"
+        $Output = Write-FunctionAppName -NameParts $NameParts -Version1
+        Write-Output $Output
+
+    }
+
+}
+elseif ($BranchName -match $V1FeatureBranchRegEx) {
+
+    Write-Verbose -Message "$BranchName is a version 1 feature branch"
+    Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version1"
+    if ($PSCmdlet.ParameterSetName -eq "Release") {
+
+        Write-Output "##vso[task.setvariable variable=DssApiVersion;isOutput=$IsOutput]v1"
+        $Output = Write-FunctionAppName -NameParts $NameParts -Version1
+        Write-Output $Output
+
+    }
+
+}
+elseif ($BranchName -match $V2OrHigherMasterBranchRegEx) {
+    
+    Write-Verbose -Message "$BranchName is a version 2 or higher master branch"
+    Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version2+"
+    if ($PSCmdlet.ParameterSetName -eq "Release") {
+
+        Write-Output "##vso[task.setvariable variable=DssApiVersion;isOutput=$IsOutput]$($Matches[1])"
+        $Output = Write-FunctionAppName -NameParts $NameParts -RegExMatches $Matches
+        Write-Output $Output
+
+    }
+
+}
+elseif ($BranchName -match $V2OrHigherFeatureBranchRegEx) {
+
+    Write-Verbose -Message "$BranchName is a version 2 or higher feature branch"
+    Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version2+"
+    if ($PSCmdlet.ParameterSetName -eq "Release") {
+
+        Write-Output "##vso[task.setvariable variable=DssApiVersion;isOutput=$IsOutput]$($Matches[1])"
+        $Output = Write-FunctionAppName -NameParts $NameParts -RegExMatches $Matches
+        Write-Output $Output
+        
+    }
+
+}
+elseif ($BranchName -match "merge") {
+
+    Write-Verbose -Message "This build was triggered by a pull request"
+    if ($PullRequestBranchName -match $V1MasterBranchRegEx) {
+
+        Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version1"
+
+    }
+    elseif ($PullRequestBranchName -match $V1FeatureBranchRegEx) {
+
+        Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version1"
+
+    }
+    elseif ($PullRequestBranchName -match $V2OrHigherMasterBranchRegEx) {
+
+        Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version2+"
+
+    }
+    elseif ($PullRequestBranchName -match $V2OrHigherFeatureBranchRegEx) {
+
+        Write-Output "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$IsOutput]Version2+"
+        
+    }
+
+}
+else {
+
+    throw "$BranchName doesn't conform to the branch naming rules and will not build"
+}
+
---
+++ Tests/U001.Scripts.Test-BranchName.Tests.ps1
@@ -0,0 +1,83 @@
+Push-Location -Path $PSScriptRoot\..\Scripts\
+
+Describe "Test-BranchName unit tests" -Tag "Unit" {
+
+
+    It "Should should throw an error given an invalid branch name" -TestCases @(
+        @{ BranchName = 'master-va'}
+        @{ BranchName = 'ABC-123-ThisIsAChange-v2'}
+        @{ BranchName = 'CDS-123-ThisIsAChange-va'}
+        @{ BranchName = 'CDS-ThisIsAChange-v2'}
+    ) {
+        param ($BranchName)
+
+        { .\Test-BranchName -BranchName $BranchName -PipelineType Build } | Should -Throw
+
+    }
+
+    It "Should write Version1 given a valid version 1 branch name" -TestCases @(
+        @{ BranchName = "master"; PipelineType = "Build"; ExpectedOutputType = "true" }
+        @{ BranchName = "master"; PipelineType = "Release"; ExpectedOutputType = "false" }
+        @{ BranchName = "CDS-101-ThisIsAChangeToV1"; PipelineType = "Build"; ExpectedOutputType = "true" }
+        @{ BranchName = "CDS-101-ThisIsAChangeToV1"; PipelineType = "Release"; ExpectedOutputType = "false" }
+    ) {
+        param ($BranchName, $PipelineType, $ExpectedOutputType)
+
+        $Expected = "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$ExpectedOutputType]Version1"
+
+        $Output = .\Test-BranchName -BranchName $BranchName -PipelineType $PipelineType
+        $Output | Should be $Expected
+    }
+
+    It "Should write Version2+ given a valid version 2 or higher branch name" -TestCases @(
+        @{ BranchName = "master-v2"; PipelineType = "Build"; ExpectedOutputType = "true" }
+        @{ BranchName = "master-v2"; PipelineType = "Release"; ExpectedOutputType = "false" }
+        @{ BranchName = "master-v3"; PipelineType = "Build"; ExpectedOutputType = "true" }
+        @{ BranchName = "master-v3"; PipelineType = "Release"; ExpectedOutputType = "false" }
+        @{ BranchName = "CDS-321-ThisIsAChangeToV2-v2"; PipelineType = "Build"; ExpectedOutputType = "true" }
+        @{ BranchName = "CDS-321-ThisIsAChangeToV2-v2"; PipelineType = "Release"; ExpectedOutputType = "false" }
+        @{ BranchName = "CDS-456-ThisIsAChangeToV3-v10"; PipelineType = "Build"; ExpectedOutputType = "true" }
+        @{ BranchName = "CDS-456-ThisIsAChangeToV3-v10"; PipelineType = "Release"; ExpectedOutputType = "false" }
+    ) {
+        param ($BranchName, $PipelineType, $ExpectedOutputType)
+
+        $Expected = "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$ExpectedOutputType]Version2+"
+
+        $Output = .\Test-BranchName -BranchName $BranchName -PipelineType $PipelineType
+        $Output | Should be $Expected
+    }
+
+    It "Should write FunctionAppVersion and FunctionAppName given a valid FunctionAppBaseName" -TestCases @(
+        @{ BranchName = "master"; PipelineType = "Release"; FunctionAppBaseName = "dss-at-foo-fa"; ApiVersion = "v1"; FunctionAppVersion = "Version1" }
+        @{ BranchName = "master-v2"; PipelineType = "Release"; FunctionAppBaseName = "dss-at-foo-fa"; ApiVersion = "v2"; FunctionAppVersion = "Version2+" }
+        @{ BranchName = "CDS-101-ThisIsAChangeToV1"; PipelineType = "Release"; FunctionAppBaseName = "dss-at-foo-fa"; ApiVersion = "v1"; FunctionAppVersion = "Version1" }
+        @{ BranchName = "CDS-456-ThisIsAChangeToV3-v3"; PipelineType = "Release"; FunctionAppBaseName = "dss-at-foo-fa"; ApiVersion = "v3"; FunctionAppVersion = "Version2+" }
+    ) {
+        param ($BranchName, $PipelineType, $ApiVersion, $FunctionAppVersion)
+
+        $FunctionAppName = "dss-at-foo-$ApiVersion-fa"
+        $Expected = @("##vso[task.setvariable variable=FunctionAppVersion;isOutput=false]$($FunctionAppVersion)",
+            "##vso[task.setvariable variable=DssApiVersion;isOutput=false]$($ApiVersion)",
+            "##vso[task.setvariable variable=FunctionAppName;isOutput=false]$FunctionAppName")
+
+        $Output = .\Test-BranchName -BranchName $BranchName -PipelineType $PipelineType -FunctionAppBaseName "dss-at-foo-fa"
+        $Output | Should be $Expected
+    }
+
+    it "Should write FunctionAppVersion based on the PullRequestBranchName param if BranchName is merge" -TestCases @(
+        @{ BranchName = "merge"; PipelineType = "Build"; PullRequestBranchName = "master"; ExpectedOutputType = "true"; FunctionAppVersion = "Version1" }
+        @{ BranchName = "merge"; PipelineType = "Build"; PullRequestBranchName = "master-v2"; ExpectedOutputType = "true"; FunctionAppVersion = "Version2+" }
+        @{ BranchName = "merge"; PipelineType = "Build"; PullRequestBranchName = "CDS-101-ThisIsAChangeToV1"; ExpectedOutputType = "true"; FunctionAppVersion = "Version1" }
+        @{ BranchName = "merge"; PipelineType = "Build"; PullRequestBranchName = "CDS-456-ThisIsAChangeToV3-v3"; ExpectedOutputType = "true"; FunctionAppVersion = "Version2+" }
+    ) {
+        param ($BranchName, $PipelineType, $PullRequestBranchName, $ExpectedOutputType, $FunctionAppVersion)
+
+        $Expected = "##vso[task.setvariable variable=FunctionAppVersion;isOutput=$ExpectedOutputType]$($FunctionAppVersion)"
+
+        $Output = .\Test-BranchName -BranchName $BranchName -PipelineType $PipelineType -PullRequestBranchName $PullRequestBranchName
+        $Output | Should be $Expected
+    }
+
+}
+
+Push-Location -Path $PSScriptRoot
