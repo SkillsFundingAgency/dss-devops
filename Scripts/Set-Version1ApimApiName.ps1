e3e248605da1e4feeee3104fd8e92b45be230641
NickGraham101 <nick.graham101@googlemail.com>
2019-03-12 15:01:14 +0000

    Added script to override ApimApiVersion

---
+++ Scripts/Set-Version1ApimApiName.ps1
@@ -0,0 +1,20 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$false)]
+    [string]$DssApiVersion,
+    [Parameter(Mandatory=$true)]
+    [string]$ApimApiName
+)
+
+# this task exists to support the null versioning identifier required for version 1
+# it can be removed when version 1 is depracated
+
+if (!$DssApiVersion) { 
+
+    Write-Verbose "Overriding ApimApiName variable"
+    $Version1ApimApiName = $ApimApiName.Replace("-", "")
+    Write-Verbose "Setting ApimApiName value to $Version1ApimApiName"
+    Write-Output "##vso[task.setvariable variable=ApimApiName]$Version1ApimApiName" 
+    Write-Verbose "ApimApiName value set to $ApimApiName"
+    
+}
