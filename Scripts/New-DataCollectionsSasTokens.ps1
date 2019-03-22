dbd884406d3bc39987373d907a336a2ffb36fbc0
NickGraham101 <nick.graham101@googlemail.com>
2019-03-22 10:47:18 +0000

    Added script to create SAS token for DC

---
+++ Scripts/New-DataCollectionsSasTokens.ps1
@@ -0,0 +1,10 @@
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$StorageAccountName,
+    [Parameter(Mandatory=$true)]
+    [string]$StorageAccountKey
+)
+
+$Context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
+New-AzureStorageContainerSASToken -Name "collections" -Permission lc -StartTime $([DateTime]::Now.ToString()) -ExpiryTime $([DateTime]::Now.AddDays(365).ToString()) -Context $Context -Verbose
