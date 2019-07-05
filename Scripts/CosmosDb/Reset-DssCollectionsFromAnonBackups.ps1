c259c7638b5ac8f21fe12d34d67a747155b46d7d
NickGraham101 <nick.graham101@googlemail.com>
2019-07-05 11:26:53 +0100

    Added documenation

--- Scripts/CosmosDb/Reset-DssCollectionsFromAnonBackups.ps1
+++ Scripts/CosmosDb/Reset-DssCollectionsFromAnonBackups.ps1
@@ -1,22 +1,49 @@
+<#
+
+    .SYNOPSIS
+    Deletes contents of DSS Cosmos collections and replaces with data from anonymised production backups
+
+    .DESCRIPTION
+    Retrieves a backup files from the anon-backup container in the DSS Production environment
+    For each backup file:
+        1. Deletes the contents of it's CosmosDb collection
+        2. Truncates the associated SQL db tables
+        3. Imports the anonymised backup file into the empty Cosmos collection
+    The SQL tables will be repopulated by the DSS change feed backend function apps
+
+    .NOTES
+    Requires https://cosmosdbportalstorage.blob.core.windows.net/datamigrationtool/2018.02.28-1.8.1/dt-1.8.1.zip to be extracted to C:\Program Files (x86)\AzureCosmosDBDataMigrationTool\
+
+#>
 [CmdletBinding()]
 param(
+    # The date stamp of the backup files to use for the restore.  The date will be take from the file name not the meta data.  
+    # If multiple backups were taken on that date then multiple restores for each collection will take place.
     [Parameter(Mandatory=$true)]    
     [DateTime]$DateToRestoreFrom,
+    # The environment to restore to.  Can only be AT, TEST or PP (values should be entered as lowercase)
     [Parameter(Mandatory=$true)]
     [ValidateSet("at", "test", "pp")]
     [String]$EnvironmentToRestoreTo,
+    # Read/Write CosmosDb key for the AT, TEST or PP CosmosDb account
     [Parameter(Mandatory=$true)]
     [String]$DestinationCosmosKey,
+    # Path to your local copy of the dfc-devops repo or the artifact if running from an Azure DevOps agent
     [Parameter(Mandatory=$true)]
     [String]$PathToDfcDevops,
+    # Path to your local copy of the dss-devops repo or the artifact if running from an Azure DevOps agent
     [Parameter(Mandatory=$true)]
     [String]$PathToDssDevops,
+    # Storage account key for the PRD storage account
     [Parameter(Mandatory=$true)]
     [String]$SourceStorageKey,
+    # The FQDN of the AT, TEST or PP SQL server
     [Parameter(Mandatory=$true)]
     [String]$SqlServerFqdn,
+    # The password of the AT, TEST or PP SQL user
     [Parameter(Mandatory=$true)]
     [String]$SqlServerPassword,
+    # The username of the AT, TEST or PP SQL user
     [Parameter(Mandatory=$true)]
     [String]$SqlServerUsername
 )
