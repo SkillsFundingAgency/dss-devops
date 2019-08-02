43035d3e788dc7ac8b9067970a1a04be251737e7
NickGraham101 <nick.graham101@googlemail.com>
2019-08-02 15:11:15 +0100

    Container executes script in WhatIf mode

--- Applications/NCS.DSS.AnonymiseBackup/PSScripts/Start-AnonymiseBackup.ps1
+++ Applications/NCS.DSS.AnonymiseBackup/PSScripts/Start-AnonymiseBackup.ps1
@@ -1,4 +1,4 @@
-[CmdletBinding()]
+[CmdletBinding(SupportsShouldProcess=$true)]
 param(
     [Parameter(Mandatory=$true)]
     [string[]]$CosmosCollections,
@@ -14,11 +14,20 @@ param(
     [string]$StorageAcountName
 )
 
+Write-Verbose "Importing module from $PathToModule"
 Import-Module $PathToModule
 
-$Keys = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroup -Name $StorageAcountName
-$Context = New-AzStorageContext -StorageAccountName $StorageAcountName -StorageAccountKey $Keys[0].Value
-$ReadSAS = New-AzStorageContainerSASToken -Permission rl -StartTime $([DateTime]::Now) -ExpiryTime $([DateTime]::Now.AddHours(2)) -Context $context -Name $SourceContainer
-$WriteSAS = New-AzStorageContainerSASToken -Permission adw -StartTime $([DateTime]::Now) -ExpiryTime $([DateTime]::Now.AddHours(2)) -Context $context -Name $DestinationContainer
+if ($PSCmdlet.ShouldProcess("$StorageAcountName", "Getting SAS tokens")) {
 
-Invoke-AnonymiseBackup -CosmosCollectionNames $CosmosCollections -DestinationContainerName $DestinationContainer -DestinationContainerSASToken $WriteSAS -SourceContainerName $SourceContainer -SourceContainerSASToken $ReadSAS -StorageAccountName $StorageAcountName -Verbose
+    $Keys = Get-AzStorageAccountKey -ResourceGroupName $ResourceGroup -Name $StorageAcountName
+    $Context = New-AzStorageContext -StorageAccountName $StorageAcountName -StorageAccountKey $Keys[0].Value
+    $ReadSAS = New-AzStorageContainerSASToken -Permission rl -StartTime $([DateTime]::Now) -ExpiryTime $([DateTime]::Now.AddHours(2)) -Context $context -Name $SourceContainer
+    $WriteSAS = New-AzStorageContainerSASToken -Permission adw -StartTime $([DateTime]::Now) -ExpiryTime $([DateTime]::Now.AddHours(2)) -Context $context -Name $DestinationContainer
+
+}
+
+if ($PSCmdlet.ShouldProcess("$CosmosCollections", "Starting Invoke-AnonymiseBackup")) {
+
+    Invoke-AnonymiseBackup -CosmosCollectionNames $CosmosCollections -DestinationContainerName $DestinationContainer -DestinationContainerSASToken $WriteSAS -SourceContainerName $SourceContainer -SourceContainerSASToken $ReadSAS -StorageAccountName $StorageAcountName -Verbose
+
+}
