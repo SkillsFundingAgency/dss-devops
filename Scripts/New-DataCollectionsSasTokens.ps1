a9f4cce135d052f446c609f442f6b26aa5a6e479
leeframpton <61736432+leeframpton@users.noreply.github.com>
2021-04-09 14:52:00 +0100

    add 'fulluri' param to New-DataCollectionsSasTokens.ps1

--- Scripts/New-DataCollectionsSasTokens.ps1
+++ Scripts/New-DataCollectionsSasTokens.ps1
@@ -7,4 +7,4 @@ param(
 )
 
 $Context = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $StorageAccountKey
-New-AzureStorageContainerSASToken -Name "collections" -Permission lcw -StartTime $([DateTime]::Now.ToString()) -ExpiryTime $([DateTime]::Now.AddDays(365).ToString()) -Context $Context -Verbose
+New-AzureStorageContainerSASToken -Name "collections" -Permission lcw -StartTime $([DateTime]::Now.ToString()) -ExpiryTime $([DateTime]::Now.AddDays(365).ToString()) -Context $Context -FullUri -Verbose
