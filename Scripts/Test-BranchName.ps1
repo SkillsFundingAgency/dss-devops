0252c5392fb96867b28bf8fa4403103e95e66fea
Joe Hudson-Small <Joehs@me.com>
2022-05-20 19:07:27 +0100

    better regex?

--- Scripts/Test-BranchName.ps1
+++ Scripts/Test-BranchName.ps1
@@ -59,10 +59,10 @@ function Write-FunctionAppName {
 
 # --- RegEx Patterns
 $V1MasterBranchRegEx = "^master$"
-$V1FeatureBranchRegEx = "^(?:CDS|SDD|NCSLT|release|master)(-\d+(-\w+)*(-(v\d+)*))*.*$"
+$V1FeatureBranchRegEx = "^(master)(-v\d+)?|(release\/).*|(CDS|SDD|NCSLT)(-\d+)(.*)(-v\d+){1}$"
 
 $V2OrHigherMasterBranchRegEx = "^master-(v\d+)$"
-$V2OrHigherFeatureBranchRegEx = "^(?:CDS|SDD|NCSLT|release|master)(-\d+(-\w+)*(-(v\d+)*))*.*$"
+$V2OrHigherFeatureBranchRegEx = "^(master)(-v\d+)?|(release\/).*|(CDS|SDD|NCSLT)(-\d+)(.*)(-v\d+){1}$"
 
 if ($PSCmdlet.ParameterSetName -eq "Release") {
 
