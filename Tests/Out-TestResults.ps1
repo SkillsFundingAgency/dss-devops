d159dd4040c4bc11aacf5a3d1726926a3eb6da10
Daniel W <dawis@netcompany.com>
2022-08-04 17:20:08 +0100

    Changed out-testresult path

--- Tests/Out-TestResults.ps1
+++ Tests/Out-TestResults.ps1
@@ -30,7 +30,7 @@ Param (
 )
 
 if (-not $TestResultFile) {
-    $FindRecentFile = Get-ChildItem "$PSScriptRoot\TEST-*.xml" |
+    $FindRecentFile = Get-ChildItem "Tests\TEST-*.xml" |
         Sort-Object LastWriteTime -Descending |
         Select-Object -First 1
     $TestResultFile = $FindRecentFile.FullName
@@ -38,7 +38,7 @@ if (-not $TestResultFile) {
 [xml] $TestResult = Get-Content -Path $TestResultFile
 
 if (-not $CodeCoverageFile) {
-    $FindRecentFile = Get-ChildItem "$PSScriptRoot\CODECOVERAGE-*.xml" |
+    $FindRecentFile = Get-ChildItem "Tests\CODECOVERAGE-*.xml" |
         Sort-Object LastWriteTime -Descending |
         Select-Object -First 1
     $CodeCoverageFile = $FindRecentFile.FullName
